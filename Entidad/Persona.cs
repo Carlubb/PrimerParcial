4d19b2f525606f0abc20e0fe1264a20c5aabb6e3
Carlubb <91338328+Carlubb@users.noreply.github.com>
2021-09-25 12:30:59 -0500

    Update Persona.cs

--- Entidad/Persona.cs
+++ Entidad/Persona.cs
@@ -12,6 +12,18 @@ namespace Entidad
             Edad = edad;
             Sexo = sexo;
         }
+        
+        public Persona()
+        {
+
+        }
+
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+        public decimal Pulsacion { get; set; }
+        public override string ToString()
     
     }
 }
