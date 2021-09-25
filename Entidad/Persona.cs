c0e081fa13868b590786c6cbb3981ce4ce3a9d4a
Carlubb <91338328+Carlubb@users.noreply.github.com>
2021-09-25 12:25:28 -0500

    Update and rename Class1.cs to Persona.cs

---
+++ Entidad/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Entidad
+{
+    public class Persona
+    {
+    
+    public Persona(string identificacion, string nombre, int edad, string sexo)
+        {
+            Identificacion = identificacion;
+            Nombre = nombre;
+            Edad = edad;
+            Sexo = sexo;
+        }
+    
+    }
+}
